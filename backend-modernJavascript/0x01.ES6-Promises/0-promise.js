77cf6dd93e3760f5e0cd97fe9d9428c82b3acda1
root <root@MOSES.localdomain>
2025-04-10 02:09:07 +0100

    feat: Create a basic Promise.

---
+++ backend-modernJavascript/0x01.ES6-Promises/0-promise.js
@@ -0,0 +1,7 @@
+// Creating a Basic Promise
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+    // This is the simpliest form of a Promise
+    // It doesn't do anything yet, but it is a valid Promise
+    });
+}
